2d38eee6f8adba76acfc86272b095bedb9e0d7ad
11277 <ying.zheng@hand-china.com>
2018-08-07 23:58:51 +0800

    [added]请求缓存、请求部分内容

--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -2,5 +2,14 @@ module.exports = {
   root: process.cwd(),
   port: 8888,
   hostname: '127.0.0.1', 
-  compress: /\.(html|js|css|md)/
+  compress: /\.(html|js|css|md)/,
+
+  cache: {
+    expires: true, 
+    maxAge: 600, // 单位秒，cache-control的时间
+    cacheControl:true, 
+    lastModified: true,
+    etag: true
+    
+  }
 }
--- src/helper/route.js
+++ src/helper/route.js
@@ -9,6 +9,8 @@ const source = fs.readFileSync(path.join(__dirname,'../template/dir.tpl'))
 const template = Handlebars.compile(source.toString())
 const mime = require('./mime.js')
 const compress = require('./compress')
+const range = require('./range')
+const isFresh = require('./cache')
 
 module.exports = async function (req, res, filePath) {
   try {
@@ -17,9 +19,26 @@ module.exports = async function (req, res, filePath) {
     if (stats.isFile()) {
       // res.writeHead(200, { 'Content-Type': mime(filePath) })
       
-      res.statusCode = 200
+      
       res.setHeader('Content-Type', mime(filePath))
-      let rs = fs.createReadStream(filePath)
+
+      if(isFresh(stats, req, res)){
+        res.statusCode = 304
+        res.end()
+        return 
+      }
+
+      let rs
+      const {code, start, end} = range(stats.size, req, res)
+      if(code === 200){
+        res.statusCode = 200
+        rs = fs.createReadStream(filePath)
+      } else {
+        res.statusCode = 206
+        rs = fs.createReadStream(filePath, {start, end})
+      }
+  
+
       if(filePath.match(config.compress)) {
         rs = compress(rs, req, res)
       }
